81c348ff65e7187ed684baad7626668cd7757498
Jose Oaxaca <jose.oaxaca@plankdesign.com>
2024-12-16 20:28:30 -0500

    get the port from env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,17 +5,22 @@ import (
 	"github.com/joaxaca/goapi/internal/handlers"
 	"github.com/joaxaca/goapi/internal/middleware"
 	"log"
+	"os"
 )
 
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/account/coins", handlers.GetCoinBalance)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
 
 	server := &http.Server{
-		Addr:    ":8000",
+		Addr:    ":" + port,
 		Handler: middleware.StripSlashes(middleware.Authorization(router)),
 	}
 
-	log.Println("Starting GO API service on port :8000...")
+	log.Println("Starting GO API service on port :" + port + "...")
 	server.ListenAndServe()
 }
